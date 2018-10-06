326b7f70cb5cc06f104483f8978e4f4d945ff88f
Megan Bailey <mkbailey391@gmail.com>
2018-10-06 14:01:37 -0700

    installs materialize, creates views folder with and index page and login page, creates passport file for authentication strategy.

--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,6 @@
+const 
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy, 
+    User = require('../models/User');
 
+    module.exports = passport;
--- routes/users.js
+++ routes/users.js
@@ -2,3 +2,5 @@ const
   express = require('express'),
   usersRouter = new express.Router(),
   passport = require('passport')
+
+  module.exports = usersRouter
