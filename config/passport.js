02e5beace4b01244692346019339129c2045d2ae
Megan Bailey <mkbailey391@gmail.com>
2018-10-06 14:25:38 -0700

    Creates login process. Requires email and password. Displays message informing a user if they do not have an account.

--- config/passport.js
+++ config/passport.js
@@ -13,6 +13,35 @@ const
         });
     });
 
+    passport.use('local-signup', new LocalStrategy({
+        usernameField: 'email', 
+        passwordField: 'password',
+        passReqToCallback: true
+    }, (req, email, password, done) =>{
+        User.findOne({ email }, (err, user) => {
+            if (err) return done(err);
+            if (user) return done(null, false, req.flash('signupMessage', "Email already used."));
+            User.create(req.body, (err, newUser) =>{
+                if (err) return console.log(err);
+                return done(null, newUser, null);
+            })
+        })
+    }));
+
+    passport.use('local-login', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, (req, email, password, done)=>{
+        User.findOne({ email }, (err, user) =>{
+            if (err) return done(err);
+            if (!user) return done(null, false, req.flash('loginMessage', "User does not exist"));
+            if (!user.isValidPassword(password)) return done(null, false, req.flash('loginMessage', "Incorrect Password"));
+            return done(null, user);
+        })
+    }))
+
+
 
 
     module.exports = passport;
