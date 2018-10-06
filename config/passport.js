9d31668021adc4a1fb494fedbd40a18ba48dd67c
Megan Bailey <mkbailey391@gmail.com>
2018-10-06 14:18:17 -0700

    Creates session keys using serialize and deserialize

--- config/passport.js
+++ config/passport.js
@@ -3,4 +3,16 @@ const
     LocalStrategy = require('passport-local').Strategy, 
     User = require('../models/User');
 
+    passport.serializeUser((user, done) =>{
+        done(null, user.id);
+    });
+
+    passport.deserializeUser((id, done) =>{
+        User.findById(id, (err, user)=>{
+            done(err, user);
+        });
+    });
+
+
+
     module.exports = passport;
