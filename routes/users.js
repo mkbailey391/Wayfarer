4b06df088448d19c587d0ade5d78135c5b26bc4a
Megan Bailey <mkbailey391@gmail.com>
2018-10-06 18:09:09 -0700

    adds edit profile to views folder. Renders edit user page

--- routes/users.js
+++ routes/users.js
@@ -24,5 +24,33 @@ const
     failureRedirect: "/users/signup"
   }));
 
+  usersRouter.get('/profile', isLoggedIn, (req, res) =>{
+    res.render('profile', { user: req.user });
+  });
+
+  usersRouter.patch('/profile', isLoggedIn, (req, res) =>{
+    if (!req.body.password) delete req.body.password;
+    Object.assign(req.user, req.body);
+    req.user.save((err, updatedUser) =>{
+      if (err) return console.log(err);
+      res.redirect('/users/profile');
+    })
+  
+  })
+  
+  usersRouter.get('/profile/edit', isLoggedIn, (req, res) =>{
+    res.render('editProfile');
+  })
+
+  usersRouter.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  })
+
+  function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()) return next ()
+    res.redirect('/users/login');
+  }
+
 
   module.exports = usersRouter;
