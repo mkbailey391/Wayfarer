b837cfe7ead90275424d8328c0f12ec18bdd1e8e
Megan Bailey <mkbailey391@gmail.com>
2018-10-11 16:07:13 -0700

    adds additional styling to site.

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(flash()); 
 app.use(methodOverride('_method'));
 app.use(express.json());
+app.use(express.static('public'))
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
